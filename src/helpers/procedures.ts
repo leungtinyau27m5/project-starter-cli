c8f8c2499c9a17c45579a4696f327817076daff1
steve.leung <steve.leung@apcapitalinvestment.com>
2024-11-19 15:34:38 +0800

    catch if hook scripts are not found

--- src/helpers/procedures.ts
+++ src/helpers/procedures.ts
@@ -7,7 +7,12 @@ export const runBeforeCreateHook = async (config: TemplateConfig) => {
   try {
     const ext = isProduction() ? "js" : "mjs";
     const file = join(config.parentPath, config.name, `beforeCreate.${ext}`);
-    await access(join(config.parentPath, config.name, `beforeCreate.${ext}`));
+    try {
+      await access(join(config.parentPath, config.name, `beforeCreate.${ext}`));
+    } catch {
+      console.info(`${config.name}: beforeCreate script is not found, skip it`);
+      return;
+    }
     const { default: hook } = await import(file);
     await hook();
   } catch (error) {
@@ -19,7 +24,12 @@ export const runAfterCreateHook = async (config: TemplateConfig) => {
   try {
     const ext = isProduction() ? "js" : "mjs";
     const file = join(config.parentPath, config.name, `afterCreate.${ext}`);
-    await access(join(config.parentPath, config.name, `afterCreate.${ext}`));
+    try {
+      await access(join(config.parentPath, config.name, `afterCreate.${ext}`));
+    } catch {
+      console.info(`${config.name}: afterCreate script is not found, skip it`);
+      return;
+    }
     const { default: hook } = await import(file);
     await hook();
   } catch (error) {
