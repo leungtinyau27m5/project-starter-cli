fba2db51f57121f1738555de586cbe07d208af6f
steve.leung <steve.leung@apcapitalinvestment.com>
2024-11-19 15:49:38 +0800

    template prompt default value with converter func

--- src/helpers/preparePrompts.ts
+++ src/helpers/preparePrompts.ts
@@ -4,12 +4,18 @@ import { TemplateConfig } from "./scanTemplates";
 export type IPrompt = {
   argv: string[];
   inquirer: DistinctQuestion;
+  converter?: (value: string) => unknown;
 };
 
 export const preparePrompts = async (templateConfigs: TemplateConfig[]) => {
   const prompts: IPrompt[] = [
     {
       argv: ["--template", "-n"],
+      converter: (v) => {
+        const idx = templateConfigs.findIndex((t) => t.name === v);
+        if (idx !== -1) return idx;
+        return 0;
+      },
       inquirer: {
         name: "template",
         type: "list",
@@ -38,7 +44,9 @@ export const preparePrompts = async (templateConfigs: TemplateConfig[]) => {
     const idx = process.argv.findIndex((a) => p.argv.includes(a));
     if (idx === -1) return p;
 
-    p.inquirer.default = process.argv[idx + 1];
+    p.inquirer.default = p.converter
+      ? p.converter(process.argv[idx + 1])
+      : process.argv[idx + 1];
 
     return p;
   });
